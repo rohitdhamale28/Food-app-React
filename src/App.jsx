c43f2e14668ed1d0174feba9be042bc894972e5a
Rohit Dhamale <rohitdhamale05@gmail.com>
2024-08-30 18:58:30 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import Navbar from "./components/navbar/navbar";
+import React, { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import Placeorder from "./pages/Placeorder/Placeorder";
 import Footer from "./components/Footer/Footer";
+import Login from "./components/Login/Login";
 const App = () => {
+  
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
     <>
+    {showLogin? <Login setShowLogin={setShowLogin}/>:<></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
